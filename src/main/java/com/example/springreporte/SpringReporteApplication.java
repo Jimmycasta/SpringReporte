28e72ea5f9b348e7530cee29b42141aff28809f7
jimmycasta <jimmycasta@gmail.com>
2023-02-02 20:42:50 -0500

    Se termina la tabla y Paginacion.

---
+++ src/main/java/com/example/springreporte/SpringReporteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReporteApplication.class, args);
+    }
+
+}
